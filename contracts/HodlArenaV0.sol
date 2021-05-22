34e7121513dbc507735c5f893fbb520a774b859a
justfear <lucmonne@gmail.com>
2021-05-22 23:52:38 +0200

    NPM init

---
+++ contracts/HodlArenaV0.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Contract for the HODL Arena
+ * @author Nobody
+ * @notice The HODL Arena contract holds all the information and logic 
+ */
+contract HodlArena is Ownable {
+    
+}
