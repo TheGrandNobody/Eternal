3cc7d1a131ffe79219a6f4a3c6714a8473b81633
Author Name <email@address.com>
2021-06-18 10:02:21 +0200

    Function comments, labeling and add lock/unlock

--- contracts/governance/OwnableEnhanced.sol
+++ contracts/governance/OwnableEnhanced.sol
@@ -20,32 +20,13 @@ import "@openzeppelin/contracts/utils/Context.sol";
  */
 abstract contract OwnableEnhanced is Context {
     address private _owner;
+    address private _previousOwner;
     address private _fund;
+    
+    uint256 private _lockPeriod;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event FundRightsAttributed(address indexed newFund);
-
-    
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    function fund() public view virtual returns (address) {
-        return _fund;
-    }
 
     /**
      * @dev Throws if called by any account other than the owner.
@@ -71,6 +52,38 @@ abstract contract OwnableEnhanced is Context {
         _;
     }
 
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+/////–––««« Variable state-inspection functions »»»––––\\\\\
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    function fund() public view virtual returns (address) {
+        return _fund;
+    }
+
+    /**
+     * @dev View the amount of time (in seconds) left before the previous owner can regain ownership
+     */
+    function getUnlockTime() public view returns (uint256) {
+        return _lockPeriod;
+    }
+
+/////–––««« Ownable-logic functions »»»––––\\\\\
+
     /**
      * @dev Leaves the contract without owner. It will not be possible to call
      * `onlyOwner` functions anymore. Can only be called by the current owner.
@@ -85,8 +98,14 @@ abstract contract OwnableEnhanced is Context {
 
     /**
      * @dev Attributes fund-rights for the Eternal Fund to a given address.
+     * @param newFund The address of the new fund 
+     *
+     * Requirements:
+     *
+     * - New owner cannot be the zero address
      */
     function attributeFundRights(address newFund) public virtual onlyOwnerAndFund {
+        require(newFund != address(0), "Ownable: new fund is the zero address");
         _fund = newFund;
         emit FundRightsAttributed(newFund);
     }
@@ -94,6 +113,11 @@ abstract contract OwnableEnhanced is Context {
     /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
+     * @param newOwner The address of the new owner
+     *
+     * Requirements:
+     *
+     * - New owner cannot be the zero address
      */
     function transferOwnership(address newOwner) public virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
@@ -101,7 +125,30 @@ abstract contract OwnableEnhanced is Context {
         _owner = newOwner;
     }
 
-    function lock (uint256 timePeriod) {
+    /**
+     * @dev Owner gives up the ownership for a given amount of time
+     * @param time The amount of time (in seconds) that the ownership is locked
+     */
+    function lockOwnership(uint256 time) public onlyOwner {
+        _previousOwner = _owner;
+        _owner = address(0);
+        _lockPeriod = block.timestamp + time;
+        emit OwnershipTransferred(_owner, address(0));
+    }
 
+    /**
+     * @dev Used to regain ownership of a previously locked contract
+     *
+     * Requirements:
+     *
+     * - Message sender must be the previous owner address
+     * - The locking period must have elapsed
+     */
+    function unlock() public {
+        require(_previousOwner == msg.sender, "Only the previous owner can unlock onwership");
+        require(block.timestamp > _lockPeriod, "The contract is still locked");
+        emit OwnershipTransferred(_owner, _previousOwner);
+        _owner = _previousOwner;
+        _previousOwner = address(0);
     }
 }
