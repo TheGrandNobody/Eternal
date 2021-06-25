f536e91e6138123a6525c1e67391019236977545
Nobody <valuepools@gmail.com>
2021-06-25 16:36:59 +0200

    Change comments from owner to admin, same for functions

--- contracts/inheritances/OwnableEnhanced.sol
+++ contracts/inheritances/OwnableEnhanced.sol
@@ -5,78 +5,81 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Context.sol";
 /**
  * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
+ * there is an account (an admin) that can be granted exclusive access to
  * specific functions.
  *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
+ * By default, the admin account will be the one that deploys the contract. This
+ * can later be changed with {transferAdminRights}.
  *
  * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
+ * `onlyAdmin`, which can be applied to your functions to restrict their use to
+ * the admin.
  *
  * @notice This is a modified version of Openzeppelin's Ownable.sol, made to add certain functionalities
- * such as different modifiers (onlyFund and onlyOwnerAndFund) and locking/unlocking
+ * such as different modifiers (onlyFund and onlyAdminAndFund) and locking/unlocking
  */
 abstract contract OwnableEnhanced is Context {
-    address private _owner;
-    address private _previousOwner;
+    address private _admin;
+    address private _previousAdmin;
     address private _fund;
     
     uint256 private _lockPeriod;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event AdminRightsTransferred(address indexed previousAdmin, address indexed newAdmin);
     event FundRightsAttributed(address indexed newFund);
 
     /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
+     * @dev Initializes the contract setting the deployer as the initial admin.
      */
     constructor () {
         address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
+        _admin = msgSender;
+        emit AdminRightsTransferred(address(0), msgSender);
     }
 
 /////–––««« Modifiers »»»––––\\\\\
     /**
-     * @dev Throws if called by any account other than the owner.
+     * @dev Throws if called by any account other than the admin.
      */
-    modifier onlyOwner() {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    modifier onlyAdmin() {
+        require(admin() == _msgSender(), "Ownable: caller is not the admin");
         _;
     }
 
     /**
-     * @dev Throws if called by any account other than the owner.
+     * @dev Throws if called by any account other than the fund.
      */
     modifier onlyFund() {
-        require(fund() == _msgSender(), "Ownable: caller is not the Eternal Fund");
+        require(fund() == _msgSender(), "Ownable: caller is not the fund");
         _;
     }
 
     /**
-     * @dev Throws if called by any account other than the owner.
+     * @dev Throws if called by any account other than the admin or the fund.
      */
-    modifier onlyOwnerAndFund() {
-        require((owner() == _msgSender()) || (fund() == _msgSender()), "Ownable: caller is not the owner or the Eternal Fund");
+    modifier onlyAdminAndFund() {
+        require((admin() == _msgSender()) || (fund() == _msgSender()), "Ownable: caller is neither the admin or fund");
         _;
     }
 
 /////–––««« Variable state-inspection functions »»»––––\\\\\
 
     /**
-     * @dev Returns the address of the current owner.
+     * @dev Returns the address of the current admin.
      */
-    function owner() public view virtual returns (address) {
-        return _owner;
+    function admin() public view virtual returns (address) {
+        return _admin;
     }
 
+    /**
+     * @dev Returns the address of the current fund.
+     */
     function fund() public view virtual returns (address) {
         return _fund;
     }
 
     /**
-     * @dev View the amount of time (in seconds) left before the previous owner can regain ownership
+     * @dev View the amount of time (in seconds) left before the previous admin can regain admin rights
      */
     function getUnlockTime() public view returns (uint256) {
         return _lockPeriod;
@@ -85,15 +88,15 @@ abstract contract OwnableEnhanced is Context {
 /////–––««« Ownable-logic functions »»»––––\\\\\
 
     /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     * @dev Leaves the contract without an admin. It will not be possible to call
+     * `onlyAdmin` functions anymore. Can only be called by the current admin.
      *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
+     * NOTE: Renouncing admin rights will leave the contract without an admin,
+     * thereby removing any functionality that is only available to the admin.
      */
-    function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
+    function renounceAdminRights() public virtual onlyAdmin{
+        emit AdminRightsTransferred(_admin, address(0));
+        _admin = address(0);
     }
 
     /**
@@ -102,53 +105,53 @@ abstract contract OwnableEnhanced is Context {
      *
      * Requirements:
      *
-     * - New owner cannot be the zero address
+     * - New admin cannot be the zero address
      */
-    function attributeFundRights(address newFund) public virtual onlyOwnerAndFund {
+    function attributeFundRights(address newFund) public virtual onlyAdminAndFund {
         require(newFund != address(0), "Ownable: new fund is the zero address");
         _fund = newFund;
         emit FundRightsAttributed(newFund);
     }
 
     /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     * @param newOwner The address of the new owner
+     * @dev Transfers admin rights of the contract to a new account (`newAdmin`).
+     * Can only be called by the current admin.
+     * @param newAdmin The address of the new admin
      *
      * Requirements:
      *
-     * - New owner cannot be the zero address
+     * - New admin cannot be the zero address
      */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
+    function transferAdminRights(address newAdmin) public virtual onlyAdmin {
+        require(newAdmin != address(0), "Ownable: new admin is the zero address");
+        emit AdminRightsTransferred(_admin, newAdmin);
+        _admin = newAdmin;
     }
 
     /**
-     * @dev Owner gives up the ownership for a given amount of time
-     * @param time The amount of time (in seconds) that the ownership is locked
+     * @dev Admin gives up admin rights for a given amount of time
+     * @param time The amount of time (in seconds) that the admin rights are given up for 
      */
-    function lockOwnership(uint256 time) public onlyOwner {
-        _previousOwner = _owner;
-        _owner = address(0);
+    function lockAdminRights(uint256 time) public onlyAdmin {
+        _previousAdmin = _admin;
+        _admin = address(0);
         _lockPeriod = block.timestamp + time;
-        emit OwnershipTransferred(_owner, address(0));
+        emit AdminRightsTransferred(_admin, address(0));
     }
 
     /**
-     * @dev Used to regain ownership of a previously locked contract
+     * @dev Used to regain admin rights of a previously locked contract
      *
      * Requirements:
      *
-     * - Message sender must be the previous owner address
+     * - Message sender must be the previous admin address
      * - The locking period must have elapsed
      */
     function unlock() public {
-        require(_previousOwner == msg.sender, "Only the previous owner can unlock onwership");
+        require(_previousAdmin == msg.sender, "Only the previous admin can unlock onwership");
         require(block.timestamp > _lockPeriod, "The contract is still locked");
-        emit OwnershipTransferred(_owner, _previousOwner);
-        _owner = _previousOwner;
-        _previousOwner = address(0);
+        emit AdminRightsTransferred(_admin, _previousAdmin);
+        _admin = _previousAdmin;
+        _previousAdmin = address(0);
     }
 }
