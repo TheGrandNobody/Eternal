cd5578fbb5ffd5430fde8755ee36c27f0efecd32
Nobody <valuepools@gmail.com>
2021-06-25 18:41:39 +0200

    Finish adding comments

--- contracts/main/FundLock.sol
+++ contracts/main/FundLock.sol
@@ -9,16 +9,21 @@ import "../interfaces/IEternalToken.sol";
  * @notice The FundLock contract holds the funds earned from the (initially) 1% fee until the Eternal Fund contract is released (2.5 months)
  */
 contract FundLock is OwnableEnhanced {
+    // The Eternal Token interface
+    IEternalToken eternal;
+
     // Keeps track of how much time has elapsed before the funds
     uint256 timeOfRelease;
 
-    IEternalToken eternal;
 
     constructor (address _eternal) {
         eternal = IEternalToken(_eternal);
         timeOfRelease = block.timestamp + 6574500;
     }
 
+    /**
+     * @dev Withraws locked funds to the Eternal Fund if 2.5 months have elapsed since deployment
+     */
     function withdrawFunds() external onlyAdminAndFund() {
         require(block.timestamp <= timeOfRelease, "Funds are still locked");
 
