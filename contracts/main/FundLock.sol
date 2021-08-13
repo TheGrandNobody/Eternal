d8591d98b0eed3a0041b70f57d6ce98766deb328
TheGrandNobody <thegrandnobody@protonmail.com>
2021-08-13 17:41:47 +0200

    Add recipient to FundLock

--- contracts/main/FundLock.sol
+++ contracts/main/FundLock.sol
@@ -13,23 +13,26 @@ contract FundLock is OwnableEnhanced {
     // The Eternal Token interface
     IEternalToken private immutable eternal;
 
+    address public immutable recipient;
+
     // Keeps track of the time at which the funds become available
     uint256 public immutable timeOfRelease;
     // Keeps track of the total amount of time to be waited
     uint256 public immutable totalWaitingTime;
 
-    constructor (address _eternal, uint256 _totalWaitingTime) {
+    constructor (uint256 _totalWaitingTime, address _eternal, address _recipient) {
         eternal = IEternalToken(_eternal);
         timeOfRelease = block.timestamp + _totalWaitingTime;
         totalWaitingTime = _totalWaitingTime;
+        recipient = _recipient;
     }
 
     /**
      * @dev Withraws locked funds to the Eternal Fund if the total waiting time has elapsed since deployment.
      */
-    function withdrawFunds() external onlyAdminAndFund() {
+    function withdrawFunds() external {
         require(block.timestamp <= timeOfRelease, "Funds are still locked");
 
-        eternal.transfer(fund(), eternal.balanceOf(address(this)));
+        eternal.transfer(recipient, eternal.balanceOf(address(this)));
     }
 }
