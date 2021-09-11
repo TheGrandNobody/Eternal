cb899b7674158fa8e9d6509db50cdbb7eecea702
TheGrandNobody <thegrandnobody@protonmail.com>
2021-09-11 18:09:27 +0200

    Generalize FundLock, so that it can also be used for vesting

--- contracts/main/EternalFund.sol
+++ contracts/main/EternalFund.sol
@@ -2,11 +2,4 @@ pragma solidity ^0.8.0;
 
 contract EternalFundV0 {
 
-    function execute(address addr) public returns(bool) {
-        bytes memory data = abi.encodePacked(true, uint256(6));
-        (bool success, bytes memory _data) = addr.call(abi.encodeWithSignature("yield(bool, uint256)", data));
-
-        return success;
-    }
-
 }
--- contracts/main/FundLock.sol
+++ contracts/main/FundLock.sol
@@ -6,7 +6,7 @@ import "../interfaces/IEternalToken.sol";
 /**
  * @title FundLock contract
  * @author Nobody (me)
- * @notice The FundLock contract holds funds for a given time period
+ * @notice The FundLock contract holds funds for a given time period. This is particularly useful for automated token vesting. 
  */
 contract FundLock is OwnableEnhanced {
 
@@ -19,20 +19,30 @@ contract FundLock is OwnableEnhanced {
     uint256 public immutable timeOfRelease;
     // Keeps track of the total amount of time to be waited
     uint256 public immutable totalWaitingTime;
+    // Keeps track of the total withdrawals the recipient can perform
+    uint256 public immutable split;
+    // Keeps track of the number of withdrawals left
+    uint256 public counter;
 
-    constructor (uint256 _totalWaitingTime, address _eternal, address _recipient) {
+    constructor (uint256 _totalWaitingTime, uint256 _split, address _eternal, address _recipient) {
         eternal = IEternalToken(_eternal);
         timeOfRelease = block.timestamp + _totalWaitingTime;
         totalWaitingTime = _totalWaitingTime;
         recipient = _recipient;
+        split = _split;
+        counter = _split;
     }
 
     /**
      * @dev Withraws locked funds to the Eternal Fund if the total waiting time has elapsed since deployment.
      */
     function withdrawFunds() external {
-        require(block.timestamp <= timeOfRelease, "Funds are still locked");
-
-        eternal.transfer(recipient, eternal.balanceOf(address(this)));
+        if (counter == 1) {
+            require(block.timestamp >= timeOfRelease, "Funds are still locked");
+        } else { 
+            counter -= 1;
+            require(block.timestamp * (10**6) >= ((timeOfRelease * (10**6)) - ((totalWaitingTime * (10**6) * counter) / split)), "Funds are still locked");
+        }
+        eternal.transfer(recipient, eternal.balanceOf(address(this)) / split);
     }
 }
