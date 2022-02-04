d2c0b2ef9dd4b856c86a59e8ddadd0db316f0c48
TheGrandNobody <thegrandnobody@protonmail.com>
2022-02-04 14:45:44 +0100

    Optimize gageLimit function

--- contracts/main/EternalFactory.sol
+++ contracts/main/EternalFactory.sol
@@ -161,11 +161,10 @@ contract EternalFactory is IEternalFactory, OwnableEnhanced {
             (, , amountAsset) = eternalTreasury.computeMinAmounts(asset, address(eternal), amountAsset, 0);
         }
 
-        uint256 _psi = eternalStorage.getUint(entity, psi);
         uint256 reserveStakedBalances = eternalStorage.getUint(treasury, keccak256(abi.encodePacked("reserveStakedBalances")));
         uint256 userStakedBalances = reserveStakedBalances - eternalStorage.getUint(treasury, keccak256(abi.encodePacked("reserveBalances", address(eternalTreasury))));
         // Available ETRNL is all the ETRNL which can be spent by the treasury on gages whilst still remaining sustainable
-        uint256 availableETRNL = eternal.balanceOf(address(eternalTreasury)) - eternalTreasury.convertToStaked(userStakedBalances) - _psi; 
+        uint256 availableETRNL = eternal.balanceOf(address(eternalTreasury)) - eternalTreasury.convertToStaked(userStakedBalances) - eternalStorage.getUint(entity, psi); 
         
         limitReached = availableETRNL < amountAsset + (2 * amountAsset * risk / (10 ** 4));
     }
